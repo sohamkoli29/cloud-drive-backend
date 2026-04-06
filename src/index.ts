9e93ebb57109ee194381c6dc331252209a1d94d6
sohamkoli29 <sohamkoli29@gmaol.com>
2026-04-06 22:57:30 +0530

    modified exceptions handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -86,12 +86,10 @@ app.listen(PORT, () => {
 // Handle uncaught exceptions
 process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
-  process.exit(1);
 });
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  process.exit(1);
 });
 
 export default app;
